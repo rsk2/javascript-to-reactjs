eb74e839f145e676a5eadf63991b128db4b5cd92
rsk2 <beingrohan24.7@gmail.com>
2019-11-29 15:14:13 +0530

    Adding new components for window size and adding headers to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.css';
 import CounterJS from './components/CounterJS';
 import CounterReactJS from './components/CounterReactJS';
+import ScreenSizeJS from './components/ScreenSizeJS';
+import ScreenSizeReactJS from './components/ScreenSizeReactJS';
 
 function App() {
   return (
     <div>
-      <CounterReactJS/>
+      <h3>Counter in React class component:</h3>
+      <CounterReactJS />
       <br/>
-      <CounterJS/>
+      <h3>Counter in vanilla JavaScript in React functional component:</h3>
+      <CounterJS />
+      <br/>
+      <h3>Window size in React class component:</h3> 
+      <ScreenSizeReactJS />
+      <br/>
+      <h3>Window size in vanilla JavaScript in React functional component:</h3> 
+      <ScreenSizeJS />
     </div>
   );
 }
